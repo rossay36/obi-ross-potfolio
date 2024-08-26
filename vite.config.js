1c1b7559292e0d6a575538c5100a298578a5151a
Ross <obi75309@gmail.com>
2024-08-26 23:12:38 +1300

    correcting somethings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		outDir: "dist", // Output directory for the build
+		sourcemap: false, // Disable sourcemaps for production
+	},
+	base: "/", // Set the base URL if necessary
+});
